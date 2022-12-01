fa7174a42ed57229b39f634271f75c00cd8285b7
msjung8634 <46917780+msjung8634@users.noreply.github.com>
2022-12-02 01:13:59 +0900

    No hierarchy
    Before using inheritance

---
+++ ArrowDamage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DamageCalculatorWithInheritance
+{
+     class ArrowDamage
+     {
+          const int BASE_DAMAGE = 3;
+          const decimal BASE_MULTIPLIER = 0.35M;
+          const decimal MAGIC_MULTIPLIER = 2.5M;
+          const decimal FLAME_DAMAGE = 1.25M;
+
+          public int Damage { get; private set; }
+
+          private int roll;
+          public int Roll
+          {
+               get { return roll; }
+               set
+               {
+                    roll = value;
+                    CalculateDamage();
+               }
+          }
+
+          private bool magic;
+          public bool Magic
+          {
+               get { return magic; }
+               set
+               {
+                    magic = value;
+                    CalculateDamage();
+               }
+          }
+
+          private bool flaming;
+          public bool Flaming
+          {
+               get { return flaming; }
+               set
+               {
+                    flaming = value;
+                    CalculateDamage();
+               }
+          }
+
+          public void CalculateDamage()
+          {
+               // Damage Equation
+               // #1. Roll * BASE_MULTIPLIER      # Always
+               // #2. #1 * MAGIC_MULTIPLIER       # Magic only
+               // #3. #2 + FLAME_DAMAGE           # Flaming only
+               // #4. Math.Ceiling(#3)            # Always
+               decimal baseDamage = Roll * BASE_MULTIPLIER;
+               if (Magic) baseDamage *= MAGIC_MULTIPLIER;
+               if (Flaming) Damage = (int)Math.Ceiling(baseDamage + FLAME_DAMAGE);
+               else Damage = (int)Math.Ceiling(baseDamage);
+          }
+
+          public ArrowDamage(int startingRoll)
+          {
+               Roll = startingRoll;
+               CalculateDamage();
+          }
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DamageCalculatorWithInheritance
+{
+     class Program
+     {
+          static Random random = new Random();
+
+          static void Main(string[] args)
+          {
+               ArrowDamage arrowDamage = new ArrowDamage(RollDice(1));
+               SwordDamage swordDamage = new SwordDamage(RollDice(3));
+
+               while (true)
+               {
+                    Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit : ");
+                    char input = Console.ReadKey(false).KeyChar;
+                    if (input != '0' && input != '1' && input != '2' && input != '3') return;
+
+                    Console.WriteLine("\nS for word, A for arrow, anything else to quit: ");
+                    char weaponKey = char.ToUpper(Console.ReadKey(false).KeyChar);
+
+                    switch (weaponKey)
+                    {
+                         case 'S':
+                              swordDamage.Roll = RollDice(3);
+                              swordDamage.Magic = (input == '1' || input == '3');
+                              swordDamage.Flaming = (input == '2' || input == '3');
+                              Console.WriteLine($"\nRolled {swordDamage.Roll} for {swordDamage.Damage} HP");
+                              break;
+                         case 'A':
+                              arrowDamage.Roll = RollDice(1);
+                              arrowDamage.Magic = (input == '1' || input == '3');
+                              arrowDamage.Flaming = (input == '2' || input == '3');
+                              Console.WriteLine($"\nRolled {arrowDamage.Roll} for {arrowDamage.Damage} HP");
+                              break;
+                         default:
+                              break;
+                    }
+               }
+
+               int RollDice(int numberOfRolls)
+               {
+                    int total = 0;
+                    for (int i = 0; i < numberOfRolls; i++)
+                         total += random.Next(1, 7);
+                    return total;
+               }
+          }
+     }
+}
